7ac1b82c0542d91fa77d6e972cdb825495c46ebf
Joshua Matern <jmatern@beyond.com>
2025-12-10 10:22:46 -0700

    Update db.py

--- google-adk-workflows-main/bebe/tools/db.py
+++ google-adk-workflows-main/bebe/tools/db.py
@@ -1,12 +1,29 @@
 # src/bebe/tools/db.py
+from google.cloud.sql.connector import Connector
+import pg8000.native
 import os
 import psycopg2
 import psycopg2.extras
 
-DATABASE_URL = os.getenv("DATABASE_URL")  # e.g., postgresql://user:pass@host/db
+
+# Connection name: project:region:instance
+INSTANCE_CONNECTION_NAME = os.getenv("qwiklabs-gcp-00-eaa89c0a0427:us-central1:bebedb")  # e.g., "my-project:us-central1:my-postgres"
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASS = os.getenv("DB_PASS", "Passwordc412345!")
+DB_NAME = os.getenv("DB_NAME", "bebedb")
+
+connector = Connector()
 
 def get_conn():
-    return psycopg2.connect(DATABASE_URL)
+    conn = connector.connect(
+        INSTANCE_CONNECTION_NAME,
+        "pg8000",
+        user=DB_USER,
+        password=DB_PASS,
+        db=DB_NAME
+    )
+    return conn
+
 
 def query_dicts(sql: str, params: dict | tuple = ()):
     with get_conn() as conn:
@@ -16,3 +33,4 @@ def query_dicts(sql: str, params: dict | tuple = ()):
 
 def query_one(sql: str, params: dict | tuple = ()):
     rows = query_dicts(sql, params)
+    
